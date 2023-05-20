391c61c2cb564e162cf5371311bb631861765f7e
Adigun-Mubaraq <adigun.mubaraq06@gmail.com>
2023-05-20 08:14:50 +0100

    V1 -- next-pastebin completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">Paste Bucket</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Button href="/create-snippet" variant="outline-info">
+            Create a Snippet
+          </Button>
+        </Nav>
+      </Navbar>
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ utils/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connect = async () => {
+  if (!isConnected) {
+    await mongoose.connect("mongodb://localhost/paste-bucket", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    isConnected = true;
+  }
+};
